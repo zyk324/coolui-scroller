6967b01f31f31eb6bc7011fe17e574e23a653994
缘℃Style <wz666s@163.com>
2021-01-29 12:04:35 +0800

    add

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import instance from './config'
-// 获取系统信息
-export const getIndex = () => {
-  return instance.get('/index/index.md')
-}
--- src/vuex/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-// import system from './modules/system'
-Vue.use(Vuex)
-export default new Vuex.Store({
-  modules: {}
-})
